36b21e8a6c1859ef26ff3c8d234269bb9b135ce9
Juaco9502 <juaco9602@hotmail.com>
2020-02-26 19:00:18 -0500

    Parte III finalizada

--- Patterns/src/main/java/edu/eci/cvds/servlet/SampleServlet2.java
+++ Patterns/src/main/java/edu/eci/cvds/servlet/SampleServlet2.java
@@ -49,4 +49,35 @@ public class SampleServlet2 extends HttpServlet{
        } 
        responseWriter.flush();
     }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       Writer responseWriter = resp.getWriter();
+       //Crear la lista
+       ArrayList<Todo> toDo=new ArrayList();
+       try{
+        Optional<String> optID = Optional.ofNullable(req.getParameter("id"));
+        String id = optID.isPresent() && !optID.get().isEmpty() ? optID.get() : "";
+        Todo todo = Service.getTodo(Integer.parseInt(id));
+		//Agregar todo a la lista de cosas por hacer
+		toDo.add(todo);
+                //OK (Funciona)
+		resp.setStatus(HttpServletResponse.SC_OK);
+		//Mostrar la tabla HTML usando Service
+		responseWriter.write(Service.todosToHTMLTable(toDo));	
+         //No encontrado (Not Found)
+       } catch(FileNotFoundException e){
+           resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+         //Requerimiento invalido (Bad Request)  
+       } catch(NumberFormatException e){
+	        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+         //Error interno en el servidor (Internal Server Error)
+       } catch(MalformedURLException e){
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+         //Para cualquier otra excepcion, devolver el código equivalente a requerimiento inválido (Bad Request)
+       } catch(Exception e){
+	        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+       } 
+       responseWriter.flush();
+    }
 }
